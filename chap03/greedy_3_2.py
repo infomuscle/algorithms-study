5a0dc295495d801f158a33b6dbeb1f7eac326d92
infomuscle <infomuscle10@gmail.com>
2022-06-22 22:39:48 +0900

    chap03 greedy

--- chap03/greedy_3_2.py
+++ chap03/greedy_3_2.py
@@ -1,14 +1,37 @@
-def solution(nums, m, k):
+import random
+
+
+def solution1(nums, m, k):
     nums.sort(reverse=True)
-    return sum([num_decision(i, k, nums[0], nums[1]) for i in range(m)])
+    return sum([_num_decision(i, k, nums[0], nums[1]) for i in range(m)])
 
 
-def num_decision(i, k, first, second):
-    return first if i == 0 or i % k != 0 else second
+def _num_decision(i, k, first, second):
+    return second if (i + 1) % (k + 1) == 0 else first
+
+
+def solution2(nums, m, k):
+    nums.sort(reverse=True)
+    count_of_unit = m // (k + 1)
+    value_of_unit = nums[0] * k + nums[1]
+    remain = m - (k + 1) * count_of_unit
+
+    return value_of_unit * count_of_unit + nums[0] * remain
 
 
 n1 = [2, 4, 5, 4, 6]
 m1 = 8
 k1 = 3
 
-print(solution(n1, m1, k1))
+n2 = [3, 5, 7, 2, 4, 6, 8, 3, 5, 7, 9, 3, 4, 5, 7, 8]
+m2 = 9
+k2 = 5
+
+for rk in range(100):
+    rn = random.randrange(100) + 1
+    s11, s12 = solution1(n1, rn, rk), solution2(n1, rn, rk)
+    s21, s22 = solution1(n2, rn, rk), solution2(n2, rn, rk)
+    if s11 != s12:
+        print("s11, s12: ", s11, s12)
+    if s21 != s22:
+        print("s21, s22: ", s21, s22)
