361e7d8d330d9b373060117544dfceb63ad96cfb
infomuscle <infomuscle10@gmail.com>
2022-06-22 23:18:46 +0900

    chap03 greedy

--- chap03/greedy_3_2.py
+++ chap03/greedy_3_2.py
@@ -27,10 +27,11 @@ n2 = [3, 5, 7, 2, 4, 6, 8, 3, 5, 7, 9, 3, 4, 5, 7, 8]
 m2 = 9
 k2 = 5
 
-for rk in range(100):
-    rn = random.randrange(100) + 1
-    s11, s12 = solution1(n1, rn, rk), solution2(n1, rn, rk)
-    s21, s22 = solution1(n2, rn, rk), solution2(n2, rn, rk)
+for i in range(100):
+    rm = random.randrange(10000) + 1
+    rk = random.randrange(10000) + 1
+    s11, s12 = solution1(n1, rm, rk), solution2(n1, rm, rk)
+    s21, s22 = solution1(n2, rm, rk), solution2(n2, rm, rk)
     if s11 != s12:
         print("s11, s12: ", s11, s12)
     if s21 != s22:
