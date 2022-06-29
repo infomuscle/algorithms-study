ddd36536d2791370731fefe29b6017726ba60f99
infomuscle <infomuscle10@gmail.com>
2022-06-30 00:56:28 +0900

    chap04 implementation

--- chap04/implementation_4_3.py
+++ chap04/implementation_4_3.py
@@ -1,15 +1,13 @@
 def solution(position):
     r, c = ord(position[0]) - 96, int(position[1])
 
-    step1 = [(0, -2), (0, 2), (2, 0), (-2, 0)]
-    step2 = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+    steps = [(-2, 1), (-2, -1), (2, 1), (2, -1), (1, -2), (1, 2), (-1, 2), (-1, -2)]
 
     possible = 0
-    for s1 in step1:
-        for s2 in step2:
-            new_position = (r + s1[0] + s2[0], c + s1[1] + s2[1])
-            if ((s1[0] == 0 and s2[0] != 0) or (s1[0] != 0 and s2[0] == 0)) and 1 <= new_position[0] <= 8 and 1 <= new_position[1] <= 8:
-                possible += 1
+    for step in steps:
+        new_position = (r + step[0], c + step[1])
+        if 1 <= new_position[0] <= 8 and 1 <= new_position[1] <= 8:
+            possible += 1
 
     return possible
 
