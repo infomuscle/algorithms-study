46d7df6270798d7133cb9ba33c04d51873c6c308
infomuscle <infomuscle10@gmail.com>
2022-07-13 00:40:33 +0900

    chap05 dfs/bfs

--- chap05/dfsbfs_5_10.py
+++ chap05/dfsbfs_5_10.py
@@ -2,18 +2,15 @@ def solution(tray):
     cnt = 0
     for i in range(len(tray)):
         for j in range(len(tray[i])):
-            if tray[i][j] == 0:
-                dfs(i, j, tray)
+            if dfs(i, j, tray):
                 cnt += 1
 
     return cnt
 
 
 def dfs(x, y, tray):
-    if not (0 <= x < len(tray) and 0 <= y < len(tray[0])):
-        return
-    if tray[x][y] == 1:
-        return
+    if not (0 <= x < len(tray) and 0 <= y < len(tray[0])) or tray[x][y] == 1:
+        return False
 
     tray[x][y] = 1
 
@@ -21,6 +18,7 @@ def dfs(x, y, tray):
     dfs(x - 1, y, tray)
     dfs(x, y + 1, tray)
     dfs(x, y - 1, tray)
+    return True
 
 
 t1 = [[0, 0, 1, 1, 0], [0, 0, 0, 1, 1], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0]]
